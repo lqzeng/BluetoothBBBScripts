e7e0ad8f428dccd938943e8b30c4e755f2c66b66
Lucas Zeng <lucasqzeng@gmail.com>
2020-05-18 21:44:04 +1000

    first commit

---
+++ BBB_BluetoothCom_v2.py
@@ -0,0 +1,134 @@
+#! /usr/bin/python
+#
+#
+#
+import gpio
+
+from bluetooth import *
+from threading import Thread
+import Adafruit_BBIO.ADC as ADC
+from time import sleep
+
+class BluetoothServer:
+
+    print('start')
+
+    def __init__(self):
+        self.sensor_count = 0
+
+    def inc_count(self):
+        self.sensor_count += 1
+
+    def toggleLed(self, target):
+        if target == 'green':
+            #print('toggle green')
+            self.green_led.toggleGpioValue()
+        elif target == 'sensor':
+            #print('toggle red')
+            self.red_led.toggleGpioValue()
+        elif target == 'receive_data':
+            #print('sending pot data')
+            self.pot_on.toggleGpioValue()
+        else:
+            print('unknown remote command')
+
+    def send_ventilation(self, client_sock):
+        print("What building are you monitoring? ")
+        building_number = input()
+
+        while True:
+            if self.pot_on.getGpioValue() == 1:
+                sens_val = ADC.read(self.analog_pin_VENTILATION)
+                print('The ventilation value is: ', sens_val)
+                sendData = " ".join([str(building_number), str(self.sensor_count), str(sens_val)])
+                client_sock.send(sendData)
+                sleep(3)
+
+    def send_sensor(self, client_sock):
+        while True:
+            if self.red_led.getGpioValue() == 1:
+                if ADC.read(self.analog_pin_SENSOR) == 1:
+                    self.inc_count()
+                    print("Traffic Count: ", self.sensor_count)
+                    #client_sock.send(str(sensor_count))
+                    sleep(3)
+                else:
+                    print("Traffic Count: ", self.sensor_count)
+                    sleep(3)
+
+    def recv_command(self, client_sock):
+        while True:
+            #print("inside recv_command")
+            data = client_sock.recv(1024).strip()
+            print("received [%s]" % data)
+            self.toggleLed(data)
+
+    def execute(self):
+
+        ADC.setup()
+
+        self.green_led = gpio.gpio(48)
+        self.green_led.setDirectionValue('out')
+        self.green_led.setGpioValue(0)
+
+        self.red_led = gpio.gpio(49)
+        self.red_led.setDirectionValue('out')
+        self.red_led.setGpioValue(0)
+
+        self.pot_on = gpio.gpio(60)
+        self.pot_on.setDirectionValue('out')
+        self.pot_on.setGpioValue(0)
+
+        self.analog_pin_VENTILATION = "P9_39"
+        self.analog_pin_SENSOR = "P9_40"
+
+
+        service_uuid = "00001101-0000-1000-8000-00805F9B34FB"
+
+        server_sock = BluetoothSocket(RFCOMM)
+        server_sock.bind(("", PORT_ANY))
+        server_sock.listen(1)
+
+        port = server_sock.getsockname()[1]
+
+        advertise_service(server_sock, "BBB", service_id = service_uuid, service_classes = [service_uuid, SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
+
+        print("awaiting RFCOMM connection on channel:%d" % port)
+
+        client_sock, client_info = server_sock.accept()
+        print("accepted connection from:", client_info)
+
+        print("\n\n\n\n\n")
+
+        #client_sock.send("HELLO ANDROID, FROM BBB") #this should send message to input stream of android
+
+
+        #data = client_sock.recv(1024).strip()
+
+        t1 = Thread(target=self.recv_command, args=(client_sock,))
+        t1.daemon=True
+        t2 = Thread(target=self.send_ventilation, args=(client_sock,))
+        t2.daemon=True
+        t3 = Thread(target=self.send_sensor, args=(client_sock,))
+        t3.daemon=True
+        t1.start()
+        t2.start()
+        t3.start()
+
+        while True:
+            try:
+                pass
+            except (KeyboardInterrupt, SystemExit):
+                print("\nexiting system ")
+                client_sock.close()
+                server_sock.close()
+                print("disconnected")
+                sys.exit()
+
+
+#
+if __name__ == '__main__':
+    server = BluetoothServer()
+    server.execute()
+
+#######################
---
+++ gpio.py
@@ -0,0 +1,60 @@
+import os
+
+class gpio:
+	gpio_directory = '/sys/class/gpio'
+
+	def __init__(self, port):
+		self.port = port
+		self.port_directory = "%s/gpio%d" % (self.gpio_directory, self.port)
+		if not os.path.isdir(self.port_directory):
+			export_file = open("%s/export" % self.gpio_directory, 'w')
+			export_file.write(str(port))
+			export_file.close()
+
+	def getActiveLowValue(self):
+		temp_name = "%s/active_low" % (self.port_directory)
+		temp_file = open(temp_name, 'r')
+		self.gpio_active_low = int(temp_file.readline())
+		temp_file.close()
+		return self.gpio_active_low
+
+	def getDirectionValue(self):
+		temp_name = "%s/direction" % (self.port_directory)
+		temp_file = open(temp_name, 'r')
+		self.gpio_direction = temp_file.readline().strip()
+		temp_file.close()
+		return self.gpio_direction
+
+	def setDirectionValue(self, arg):
+		self.gpio_direction = arg
+		temp_name = "%s/direction" % (self.port_directory)
+		temp_file = open(temp_name, 'w')
+		temp_file.write(arg)
+		temp_file.close()
+
+	def getEdgeValue(self):
+		temp_name = "%s/edge" % (self.port_directory)
+		temp_file = open(temp_name, 'r')
+		self.gpio_edge = temp_file.readline().strip()
+		temp_file.close()
+		return self.gpio_edge
+
+	def getGpioValue(self):
+		temp_name = "%s/value" % (self.port_directory)
+		temp_file = open(temp_name, 'r')
+		self.gpio_value = int(temp_file.readline())
+		temp_file.close()
+		return self.gpio_value
+
+	def setGpioValue(self, arg):
+		self.gpio_value = arg
+		temp_name = "%s/value" % (self.port_directory)
+		temp_file = open(temp_name, 'w')
+		temp_file.write(str(arg))
+		temp_file.close()
+
+	def toggleGpioValue(self):
+		if self.gpio_value < 1:
+			self.setGpioValue(1)
+		else:
+			self.setGpioValue(0)
